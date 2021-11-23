62549e8e5fa7eb6687a9ea0a85979d0e2098e3e3
Dakota States <dakota.states@gmail.com>
2021-11-22 16:23:39 -0800

    Bootstrap template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
+    admin_root_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
